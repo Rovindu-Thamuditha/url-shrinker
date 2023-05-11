6014716608af0b35894a960395592c942946cc11
Rovindu Thamuditha <rovindu2007@gmail.com>
2023-05-11 17:30:05 +0530

    Added Multiple Provider Selecting Menu

--- window.py
+++ window.py
@@ -15,24 +15,32 @@ class MainWindow(tk.Tk):
 
         #StrVariables
         self.long_url = tk.StringVar(self)
-        self.provider = tk.StringVar(self)
-        self.provider.set("cuttly") #Default Option
+        self.selected_provider = tk.StringVar(self)
+        self.selected_provider.set("cuttly") #Default Option
+
+        host_providers = ["cuttly", "adfocus", "shrinkearn", "v2links", "bitly"]
 
         #Setting up frames
         self.main_frame = tk.Frame()
         self.main_frame.pack()
 
-        #URL Input Box
+
         self.entry_label = tk.Label(self.main_frame, text="Enter your long url:", font=('Roboto', 12))
         self.entry_label.grid(row=0, column=0, padx=0, pady=12)
+
+        #Host Provider
+        self.host_menu = tk.OptionMenu(self.main_frame, self.selected_provider, *host_providers)
+        self.host_menu.grid(row=1, column=0, padx=0, pady=3)
+
+        #URL Input Box
         self.long_url_in = tk.Entry(self.main_frame, width=40, textvariable=self.long_url, font=('Roboto', 12))
         #self.long_url_in.bind("<Button-1>", self.copy_url)
-        self.long_url_in.grid(row=1, column=0, padx=0, pady=6)
+        self.long_url_in.grid(row=2, column=0, padx=0, pady=6)
 
         #Shrink Button
         self.shrinkButton = tk.Button(self.main_frame, text="Shrink!", command=self.shrink_url, font=('Roboto', 11))
         self.shrinkButton.bind("Enter", self.press_enter)
-        self.shrinkButton.grid(row=2, column=0, padx=0, pady=5)
+        self.shrinkButton.grid(row=3, column=0, padx=0, pady=5)
         
         '''
         #Shortened URL
@@ -43,7 +51,8 @@ class MainWindow(tk.Tk):
 
     def get_url(self):
         self.long_url_str = self.long_url.get()
-        self.shrinked_url = shortner.select_host(host='cuttly', url=self.long_url_str)
+        self.provider = self.selected_provider.get()
+        self.shrinked_url = shortner.select_host(host=self.provider, url=self.long_url_str)
         return self.shrinked_url
 
     def shrink_url(self):
